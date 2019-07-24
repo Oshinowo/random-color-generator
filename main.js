6b25c2d349881482f8fe97ed3c574fbbbaba1669
Oshinowo Abdulazeez <abdulazeezoshinowo2@gmail.com>
2019-07-24 23:07:27 +0100

    fourth commit

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-const originalButton = document.getElementById('original-button');
-const mysteryButton = document.getElementById('mystery-button');
+var originalButton = document.getElementById('original-button');
+var mysteryButton = document.getElementById('mystery-button');
 
 //random number function
-function rgb(num) {
+function randomNumber(num) {
     return Math.floor(Math.round() * num);
 }
 
 // color changing function
-function colorChange() {
-    const randomColor = "rgb(" + rgb(255) + "," + rgb(255) + "," + rgb(255) + ")";
-    event.target.style.backgroundColor = randomColor;
+function colorChange(event) {
+    var randomColor = "rgb(" + randomNumber(255) + "," + randomNumber(255) + "," + randomNumber(255) + ")";
+    Event.target.style.backgroundColor = randomColor;
 }
+
+originalButton.onclick = colorChange;
+mysteryButton.onwheel = colorChange;
