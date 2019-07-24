a36ce4517a492ce66ab0ad978038a45c1f0b8e8a
Oshinowo Abdulazeez <abdulazeezoshinowo2@gmail.com>
2019-07-24 21:54:43 +0100

    fourth commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const originalButton = document.getElementById('original-button');
+const mysteryButton = document.getElementById('mystery-button');
+
+//random number function
+
+
+//  const randomColor = 'rgb("+ num + num + num +")';
